0365427fcc1863fb4d60db8a2a64b50fbb5cbb2c
AzizStudent <aziz.rustamov.mail@gmail.com>
2025-05-23 14:28:28 +0500

    options of db tables

--- init.sql
+++ init.sql
@@ -1,27 +1,38 @@
-CREATE TABLE IF NOT EXISTS notes (
-    id UUID PRIMARY KEY,
-    title TEXT NOT NULL,
-    content TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TYPE order_status AS ENUM (
+  'pending_payment', 'paid', 'processing', 'shipped', 'delivered', 'cancelled'
 );
-CREATE TABLE IF NOT EXISTS tasks (
-    id UUID PRIMARY KEY,
-    title TEXT NOT NULL,
-    description TEXT,
-    status TEXT CHECK (
-        status IN ('pending', 'in_progress', 'completed')
-    ) NOT NULL,
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    status order_status NOT NULL,
+    delivery_date DATE,
+    pickup_point VARCHAR(255),
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    total_amount INTEGER NOT NULL,
+    receipt_url TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-CREATE TABLE IF NOT EXISTS users (
-    id UUID PRIMARY KEY,
-    username TEXT UNIQUE NOT NULL,
-    hashed_password TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-CREATE TABLE IF NOT EXISTS sessions (
-    token UUID PRIMARY KEY,
-    user_id UUID REFERENCES users(id),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+CREATE OR REPLACE FUNCTION update_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = CURRENT_TIMESTAMP;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_orders_updated_at
+BEFORE UPDATE ON orders
+FOR EACH ROW
+EXECUTE FUNCTION update_timestamp();
+
+CREATE TABLE order_items (
+    id SERIAL PRIMARY KEY,
+    order_id INTEGER NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
+    product_id INTEGER NOT NULL REFERENCES products(id),
+    quantity INTEGER NOT NULL CHECK (quantity > 0),
+    price INTEGER NOT NULL,
+    total_price INTEGER GENERATED ALWAYS AS (quantity * price) STORED
 );
